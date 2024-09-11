e2d1207ff48a3317e1bb8f5774bddfd4f80c325c
ShumsAn <shum-sol@outlook.com>
2024-09-11 19:16:27 +0300

    "Форматирование строк".
    Цель задания:
    Освоить различные методы форматирования строк в Python.
    Научиться применять эти методы в контексте описания соревнования. История: соперничество двух команд - Мастера кода и Волшебники данных.

---
+++ module7_strings.py
@@ -0,0 +1,25 @@
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+challenge_result = 'Победа команды Волшебники данных!'
+
+
+# Использование %:
+print("В команде %(name_team1)s: '%(team1_num)x участников!'" % {'team1_num': 5, 'name_team1': 'Мастер кода'})
+print('Итого сегодня в командах участников: "%(team1_num)x и %(team2_num)x".' % {'team1_num': 5, 'team2_num': 6})
+
+# Использование format():
+print("Команда {name_team2} решила задач: '{0}'!".format(score_2, name_team2='Волшебники данных'))
+print("{} решили задачи за {} c ".format('Волшебники данных', team2_time))
+
+# Использование f-строк:
+print(f'Команды решили: "{score_1} и {score_2} задач".')
+print(f'Результат битвы: "{challenge_result}"')
+print(f"Сегодня было решено: '{tasks_total} задач, в среднем по {time_avg} секунды на задачу!'.")
+
+
